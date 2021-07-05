7dbd4409206bddcb29e139b2be6ca48b166fffe4
Imam Robani <imamrob@gmail.com>
2021-07-06 05:34:36 +0700

    setup fonts to all project

--- src/consts/Fonts.js
+++ src/consts/Fonts.js
@@ -1,7 +1,10 @@
 const Fonts = {
+  POPPINS_BOLD: 'Poppins-Bold',
+  POPPINS_ITALIC: 'Poppins-Italic',
   POPPINS_LIGHT: 'Poppins-Light',
   POPPINS_MEDIUM: 'Poppins-Medium',
-  POPPINS_REGULAR: 'Poppins-Regular'
+  POPPINS_REGULAR: 'Poppins-Regular',
+  POPPINS_SEMIBOLD: 'Poppins-SemiBold',
 }
 
 export default Fonts
