beff9a89c6e328bb51dee0214979e06cc16ed76d
Imam Robani <imamrob@gmail.com>
2021-07-06 17:31:21 +0700

    add loading component global

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,9 +1,11 @@
 import Header from './Header'
 import CardContact from './CardContact'
 import AddButton from './AddButton'
+import Loading from './Loading'
 
 export {
   Header,
   CardContact,
-  AddButton
+  AddButton,
+  Loading
 }
