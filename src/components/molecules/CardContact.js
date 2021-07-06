d89a88365b6e7997e8a2b927f0c40bbcc8dd2809
Imam Robani <imamrob@gmail.com>
2021-07-06 07:07:22 +0700

    add CardContact component

---
+++ src/components/molecules/CardContact.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { ProfileDummy } from '../../assets'
+import { Colors, Fonts } from '../../consts'
+import { Scale } from '../../utils'
+
+const CardContact = ({ photo = ProfileDummy, name, age }) => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={photo}
+        style={styles.image}
+      />
+      <View style={styles.content}>
+        <Text style={styles.title}>Name: {name}</Text>
+        <Text>Age: {age}</Text>
+      </View>
+    </View>
+  )
+}
+
+export default CardContact
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    alignItems: 'center',
+    padding: 16,
+    marginHorizontal: 24,
+    marginVertical: 8
+  },
+  image: {
+    width: 64,
+    height: 64,
+    borderRadius: 8,
+    overflow: 'hidden',
+    marginRight: 12
+  },
+  content: {
+    flex: 1
+  },
+  title: {
+    fontFamily: Fonts.POPPINS_REGULAR,
+    color: Colors.black,
+    fontSize: Scale(12)
+  },
+})
--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,5 +1,7 @@
 import Header from './Header'
+import CardContact from './CardContact'
 
 export {
-  Header
+  Header,
+  CardContact
 }
